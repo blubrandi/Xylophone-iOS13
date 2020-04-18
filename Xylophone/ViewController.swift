def24351f8baf03608fed1d6f829e68b48b13780
Brand <blubrandi0@gmail.com>
2020-04-18 14:57:50 -0600

    Quick Xylophone app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyTapped(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!, button: sender)
+    }
     
-
+    func playSound(soundName: String, button: UIButton) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        button.alpha = 0.5
+        player.play()
+    }
 }
+
 
